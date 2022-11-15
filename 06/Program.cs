0168c70a663d19a6b5b2dca821d4a569b3cbc363
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-15 23:12:31 +0300

    Lesson 01

---
+++ 06/Program.cs
@@ -0,0 +1,5 @@
+﻿// 6. Написать программу вычисления знвчения функции y = sin(a). (Класс math).
+
+double a=Convert.ToDouble(Console.ReadLine());
+double y=Math.Sin(a);
+System.Console.WriteLine(y);
