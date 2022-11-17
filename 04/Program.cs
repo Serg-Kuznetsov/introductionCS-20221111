3b094609efe396051b511428f4d6ca053ad3912e
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-17 22:55:25 +0300

    4 и 5 задачи

---
+++ 04/Program.cs
@@ -0,0 +1,13 @@
+﻿// 4.	По заданному с клавиатуры номеру дня недели вывести его название
+System.Console.WriteLine("Введите число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1) System.Console.WriteLine("Понедельник");
+if (a == 2) System.Console.WriteLine("Вторник");
+if (a == 3) System.Console.WriteLine("Среда");
+if (a == 4) System.Console.WriteLine("Четверг");
+if (a == 5) System.Console.WriteLine("Пятница");
+if (a == 6) System.Console.WriteLine("Суббота");
+if (a == 7) System.Console.WriteLine("Воскресенье");
+
+if (a > 7) System.Console.WriteLine("Нужно ввести именно от 1 до 7");
---
+++ 05/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5.	С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+System.Console.WriteLine("Введите число №1:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число №2:");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число №3:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+System.Console.WriteLine($"{max} - максимальное число");
