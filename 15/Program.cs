c8f042097a3501fbce0634ca3c3fccb8ae75673a
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-18 00:21:06 +0300

    15 задачу сделал, пошел спать, завтра работать..

---
+++ 15/Program.cs
@@ -0,0 +1,9 @@
+﻿// 15.	С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+
+System.Console.WriteLine("Введите целое число число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<99) System.Console.WriteLine("NO");
+if (a>99) System.Console.WriteLine(a%10%10);
+if (a>999) System.Console.WriteLine("Данная программа не поддерживает четырехзначные числа :)");
+
+
