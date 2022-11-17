21585cea55beb7cfd4799aab360fa1939df6d190
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-17 23:55:25 +0300

    10 задачу сделал

---
+++ 10/Program.cs
@@ -0,0 +1,6 @@
+﻿//10.	Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+System.Console.WriteLine("Введите целое число число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(a%10);
