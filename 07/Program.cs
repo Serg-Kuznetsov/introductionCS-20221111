edceed9294d045edec358ea2ddb4416cbd93dbf6
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-17 23:10:58 +0300

    7 задачу сделал

---
+++ 07/Program.cs
@@ -0,0 +1,7 @@
+﻿// 7.	Выяснить является ли число чётным.
+
+System.Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a% 2 == 0) System.Console.WriteLine("Введенное число является четным");
+else
+System.Console.WriteLine("Введенное число является нечетным");
