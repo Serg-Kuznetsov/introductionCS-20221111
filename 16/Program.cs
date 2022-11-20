bd62c38f4cca2d3167b664c0246bf4e0997feb12
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-20 21:18:49 +0300

    Lesson2

---
+++ 16/Program.cs
@@ -0,0 +1,11 @@
+﻿// 16. Дано число. Проверить кратно ли оно 7 и 23
+int n=Convert.ToInt32(Console.ReadLine());
+bool d1=n%7==0;
+bool d2=n%23==0;
+if(d1 && d2) System.Console.WriteLine("YES"); else System.Console.WriteLine("NO");
+
+
+
+// if(n%7==0 && n%23==0) System.Console.WriteLine("YES"); else System.Console.WriteLine("NO");
+
+
