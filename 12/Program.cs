5250c4df56bd782c8f09088956bba1c0e3dd26a1
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-20 18:30:48 +0300

    Решена 12 задача

---
+++ 12/Program.cs
@@ -0,0 +1,21 @@
+﻿// 12.	С клавиатуры вводится целое число из диапазона 10, 99].Показать наибольшую цифру числа/
+
+int n;
+do
+{
+System.Console.WriteLine("Введите число от 10 до 99");
+n=Convert.ToInt32(Console.ReadLine());
+}
+while (n<10 || n>99);
+
+int a=n/10;
+int b=n%10;
+int max;
+if (a>b) max=a;
+else max=b;
+
+System.Console.WriteLine($"Max = {max}");
+
+ 
+
+
