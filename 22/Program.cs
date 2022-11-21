4e2b8d1b94050b838b604a0e8d9bb2056485d8ef
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-21 23:11:26 +0300

    Сделал 22 задачу, но мозгов хватило только на трехзначное число

---
+++ 22/Program.cs
@@ -0,0 +1,9 @@
+﻿// 22.	Программа проверяет пятизначное число на палиндромом.
+System.Console.WriteLine("Введите трехзначное");
+int a = Convert.ToInt32(Console.ReadLine());
+int x1,x3;
+x1=a/100; //1 цифра
+x3=a % 10; // 3 цифра
+if (x1==x3) System.Console.WriteLine("Является палиндромом");
+else 
+System.Console.WriteLine("НЕ является палиндромом");
