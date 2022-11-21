5a56728cc582b03ca61326d1b55c18f4acd6f54b
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-21 22:26:21 +0300

    Сделал 17 задачу

---
+++ 17/Program.cs
@@ -0,0 +1,7 @@
+﻿//17.	Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+System.Console.WriteLine("Введите число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>=1 && a<=5) System.Console.WriteLine("Рабочий день");
+else
+System.Console.WriteLine("Выходной!");
