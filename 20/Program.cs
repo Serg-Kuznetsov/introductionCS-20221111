134474b3a78a218f8d13f9739af20fdf2db27f83
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 00:16:59 +0300

    Сделали несколько задач перед перерывом

---
+++ 20/Program.cs
@@ -0,0 +1,37 @@
+﻿// 20.	Определить номер четверти плоскости, в которой находится точка с координатами Х и У, 
+//причем X ≠ 0 и Y ≠ 0
+
+// тип подпрограммы имя(аргументы)
+/*class Programm
+static int Part(double x, double y) // Подпрограмма   
+{
+// Тело подпрограммы
+{
+if (x>0 && y>0) return 1;
+if (x<0 && y>0) return 2;
+if (x<0 && y<0) return 3;
+if (x>0 && y<0) return 4;
+return 0;
+}
+
+/*void Pause()
+{
+    System.Console.WriteLine("Press any key");
+    Console.Readkey();
+}
+*/
+
+/*void Pause(string message); // перегрузка по аргументам
+{
+    System.Console.WriteLine(message);
+     Console.Readkey();
+}
+
+
+
+double x=Convert.ToDouble(Console.ReadLine());
+double y=Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Part:"+Part(x,y));
+}
+*/
---
+++ 28/Program.cs
@@ -0,0 +1,19 @@
+﻿// 28.	Определить количество цифр в числе. Сделать подпрограмму.
+
+int CountDigits(int number)
+// Подпрограмма
+{
+    number = Math.Abs(number);
+    int count=0;
+    if (number==0) count=1;
+    while (number>0)
+    {
+        number=number/10;
+        count++;
+    }
+    return count;
+}
+
+// Основная программа
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(CountDigits(N));
