462b62e52725990f58c4bcdfb8c2a99a511a0461
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 22:49:37 +0300

    Сделал 25 задачу

---
+++ 25/Program.cs
@@ -0,0 +1,6 @@
+﻿// 25.	Вывести на экран кубы чисел от 1 до N
+
+System.Console.WriteLine("Введите любое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+System.Console.WriteLine($"{i} в кубе = {i*i*i}");
