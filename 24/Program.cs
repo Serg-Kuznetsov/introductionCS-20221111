1ff665d6c747f46cb3adae513dd11afac7458164
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 22:44:58 +0300

    Сделал 24 задачу

---
+++ 24/Program.cs
@@ -0,0 +1,9 @@
+﻿// 24.	Вывести на экран таблицу квадратов чисел от 1 до N
+
+System.Console.WriteLine("Введите любое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+System.Console.WriteLine($"{i} в квадрате = {i*i}");
+
+
+
