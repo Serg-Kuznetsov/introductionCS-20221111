c77e0fc996fa0deb6f5a83d4e542ffe40db00015
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 22:02:20 +0300

    Доделал задачи лекций

--- 31/Program.cs
+++ 31/Program.cs
@@ -1,12 +1,17 @@
 ﻿// 31. Сгенерировать 10 случайных чисел и показать те, которые заканчиваются на четные цифры.
 //Показать кубы чисел, заканчивающихся на четную цифру/
+bool Test (int a)
+{
+  //if (a%2==0) return true; else return false;
+  return a%2==0;
+}
 
 Random random=new Random();
 for (int i=0;i<5;i++)
 {
 int a=random.Next(1,100);
 int b=(int)Math.Pow(a,3);
-if (b%2==0)
+if (Test (b))
 System.Console.WriteLine($"{a}, {b}");
 }
 
---
+++ BasicAlgorithms/004 - Equlid/Program.cs
@@ -0,0 +1,37 @@
+﻿// Алгоритм Евклидint a=10
+int k=0;
+int GCD(int a,int b)
+{
+while(a!=b)
+{
+    k++;
+    if(a>b)
+        a=a-b;
+    else
+         b=b-a;
+ }
+   return a;
+}
+ 
+    System.Console.WriteLine(GCD(15,60));
+    System.Console.WriteLine(k);
+
+int quikGCD(int a,int b)
+
+    {
+while(a!=0 && b!=0)
+{
+    k++;
+    if(a>b)
+        a=a%b;
+    else
+         b=b%a;
+ }
+   return a+b;
+}
+
+ System.Console.WriteLine(GCD(15,60));
+ System.Console.WriteLine(k);
+ k=0;
+ System.Console.WriteLine(quikGCD(15,60));
+ System.Console.WriteLine(k);
