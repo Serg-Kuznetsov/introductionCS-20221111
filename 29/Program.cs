1754f9d6408cfab39e652bf4b9dbf5609b534e29
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-25 00:22:16 +0300

    Сделал 29 задачу

---
+++ 29/Program.cs
@@ -0,0 +1,11 @@
+﻿// 29.	Подсчитать сумму цифр в числе
+
+System.Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while (a>0)
+{
+sum = sum+a%10;
+a=a/10;
+}
+System.Console.WriteLine(sum);
