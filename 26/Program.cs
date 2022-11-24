38b18c193d55fed0415c63cfa18ec09a0ae0d940
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 23:44:13 +0300

    Сделал 26 задачу

---
+++ 26/Program.cs
@@ -0,0 +1,9 @@
+﻿// 26.	Найти сумму чисел от 1 до А
+
+System.Console.WriteLine("Введите любое целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+for(int i=0;i<a;i++)
+sum=(a*(a+1))/2;
+System.Console.WriteLine(sum);
+
