dc657bd0ef908edd2bf01e9aaeff694fa15c0af9
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-11-24 23:48:25 +0300

    Сделал 26 задачу

--- 26/Program.cs
+++ 26/Program.cs
@@ -5,5 +5,5 @@ int a = Convert.ToInt32(Console.ReadLine());
 int sum=0;
 for(int i=0;i<a;i++)
 sum=(a*(a+1))/2;
-System.Console.WriteLine(sum);
+System.Console.WriteLine($"Сумма всех чисел от 1 до {a} = {sum}");
 
