491b4b20acf6b89597373c2e1530ee98c5b7c31a
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-12-06 22:52:57 +0300

    Сделал 70 задачу

---
+++ 70/Program.cs
@@ -0,0 +1,17 @@
+﻿// 70.	Показать натуральные числа от 1 до N, N задано
+
+
+System.Console.WriteLine("Введите любое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Natural (int n, int i)
+{
+    if(i<=n)
+    {
+        System.Console.Write($"{i}  ");
+        Natural(n,i+1);
+    }
+
+}
+Natural(n,1);
+
