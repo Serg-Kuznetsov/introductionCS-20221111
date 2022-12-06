7b2d6ecd442b7126134745dc8cf81ec622100ba3
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-12-06 23:30:33 +0300

    Сделал 77 задачу

---
+++ 77/Program.cs
@@ -0,0 +1,15 @@
+﻿// 77.	Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+ //Первые два элемента последовательности 0 и 1 
+
+System.Console.WriteLine("Задайте длинну массива");
+int n=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+int[] a=new int[n];
+a[0]=0;
+a[1]=1;
+for(int i=2;i<n;i++)
+a[i]=a[i-1]+a[i-2];
+for(int i=0;i<n;i++)
+System.Console.Write($"{a[i]}  ");
+
