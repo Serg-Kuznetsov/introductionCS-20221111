be24f021417df09133497d1f62f847b51b1fce30
Sergey Kuznetsov <Sacred-box@yandex.ru>
2022-12-11 19:39:46 +0300

    прорешали задачи 9 семинара

---
+++ 84/Program.cs
@@ -0,0 +1,33 @@
+﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
+//Вычислить сумму цифр введенного числа
+
+
+int Sumdigits (int n)
+{
+int sum=0;
+n=Math.Abs(n);
+while(n!=0)
+{
+    sum=sum+n%10;
+    n=n/10;
+}
+return sum;
+}
+
+
+
+string  s=Console.ReadLine();
+int n;
+bool flag=int.TryParse(s, out n);
+if (flag)
+
+{
+
+
+// foreach(char c in s)
+//     if(char.IsDigit(c))
+//     //sum=sum+(int)char.GetNumericValue(c);
+//     sum=sum+(c-'0');
+System.Console.WriteLine($"{n} {Sumdigits(n)}");
+}
+else System.Console.WriteLine("Wrong number");
